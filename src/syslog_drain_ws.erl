91165582c1cb9eaa1b469927e0b02333f909ec1c
Cameron Bytheway <cameron@nujii.com>
2013-03-30 14:02:42 -0600

    Respond to ws and http on the same handler

--- src/syslog_drain_ws.erl
+++ src/syslog_drain_ws.erl
@@ -7,7 +7,6 @@
 -export([start_server/4]).
 -export([stop_listener/1]).
 -export([set_env/3]).
--export([get_value/3]).
 
 %% @doc Start a syslog drain listener.
 -spec start_server(any(), non_neg_integer(), any(), any()) -> {ok, pid()}.
@@ -15,8 +14,7 @@ start_server(Ref, NbAcceptors, TransOpts, ProtoOpts)
     when is_integer(NbAcceptors), NbAcceptors > 0 ->
   Dispatch = cowboy_router:compile([
     {'_', [
-      {"/", syslog_drain_ws_http_handler, []},
-      {"/ws", syslog_drain_ws_handler, []}
+      {'_', syslog_drain_ws_handler, []}
     ]}
   ]),
   cowboy:start_http(Ref, NbAcceptors, TransOpts,
